c42fa2ebaeb73382743495b9d49ccd8d1ad13297
Joan Seva Bernabeu <joan.seva@iesdoctorbalmis.com>
2021-01-15 01:12:42 +0100

    Acabo el proyecto

--- Juego_DINT/MainWindow.xaml.cs
+++ Juego_DINT/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static Juego_DINT.Pelicula;
 
 namespace Juego_DINT
 {
@@ -24,21 +25,23 @@ namespace Juego_DINT
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Pelicula> peliculas;
-        Pelicula pelicula;
+        private ObservableCollection<Pelicula> peliculas;
+        private ObservableCollection<Pelicula> partida;
+        private Pelicula pelicula;
+        Random rnd = new Random();
+
+        int numero, puntuacion;
         public MainWindow()
         {
             InitializeComponent();
-            int numero = 0;
-            pelicula = new Pelicula();
-            gestionarGrid.DataContext = pelicula;
             nombresComboBox();
             limpiarGestionar();
+            peliculas = new ObservableCollection<Pelicula>();
         }
         private void nombresComboBox()
         {
             List<string> generos = new List<string>() { "Comedia","Miedo","Accion","Misterio"};
-            gestionarComboBox.ItemsSource = generos;
+            generogestionarComboBox.ItemsSource = generos;
         }
         private void limpiarGestionar()
         {
@@ -49,18 +52,21 @@ namespace Juego_DINT
             medioRadioButton.IsChecked = false;
             dificilRadioButton.IsChecked = false;
         }
-
         private void cargarButton_Click(object sender, RoutedEventArgs e)
         {
-            string peliculasString;
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if(openFileDialog.ShowDialog() == true)
             {
-                //var list = JsonConvert.DeserializeObject<List<Item>>(Yourjson);
-                peliculasString = File.ReadAllText(openFileDialog.FileName);
+                string peliculasJson = File.OpenText(openFileDialog.FileName).ReadToEnd();
+                ObservableCollection<Pelicula> aux = JsonConvert.DeserializeObject<ObservableCollection<Pelicula>>(peliculasJson);
+
+                for(int i = 0; i < peliculas.Count; i++)
+                {
+                    Pelicula p = aux.ElementAt(i);
+                    peliculas.Add(p); 
+                }
             }
         }
-
         private void guardarButton_Click(object sender, RoutedEventArgs e)
         {
             string peliculasString = JsonConvert.SerializeObject(peliculas);
@@ -70,20 +76,98 @@ namespace Juego_DINT
                 File.WriteAllText(saveFileDialog.FileName, peliculasString);
             }
         }
-
         private void addButton_Click(object sender, RoutedEventArgs e)
         {
-            Pelicula aux = pelicula;
-            peliculas.Add(aux);
-            MessageBox.Show("Pelicula insertada con exito!");
-            pelicula = new Pelicula();
+            string titulo = titulogestionarTextBox.Text;
+            string pista = pistagestionarTextBox.Text;
+            string imagen = imagengestionarTextBox.Text;
+            bool facil = (bool)facilRadioButton.IsChecked;
+            bool medio = (bool)medioRadioButton.IsChecked;
+            bool dificil = (bool)dificilRadioButton.IsChecked;
+            Genero genero = (Genero)generogestionarComboBox.SelectedItem;
+            Pelicula pelicula = new Pelicula(titulo, pista, imagen, facil, medio, dificil, genero);
+            peliculas.Add(pelicula);
             limpiarGestionar();
         }
-
+        private void eliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            peliculas.Remove((Pelicula)peliculasListBox.SelectedItem);
+        }
         private void pistaCheckBox_Click(object sender, RoutedEventArgs e)
         {
             if ((bool)pistaCheckBox.IsChecked) pistaTextBlock.Visibility = Visibility.Visible;
             else pistaTextBlock.Visibility = Visibility.Hidden;
+        }
+        private void examinarButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true) imagengestionarTextBox.Text = openFileDialog.FileName;
+        }
+        private void flechaDerechaImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(numero < partida.Count - 1)
+            {
+                numero = numero + 1;
+                actualizar(partida[numero]);
+            }
+        }
+        private void flechaIzqImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(numero > 0)
+            {
+                numero = numero - 1;
+                actualizar(partida[numero]);
+            }
+        }
+        private void validarButton_Click(object sender, RoutedEventArgs e)
+        {
+            pelicula = partida[numero];
+            if(pelicula.titulo == tituloTextBox.Text)
+            {
+                if ((bool)pistaCheckBox.IsChecked)
+                {
+                    puntuacion = puntuacion + pelicula.puntuacion + 3;
+                    puntuacionTextBox.Text = puntuacion.ToString();
+                }
+                else
+                {
+                    puntuacion = puntuacion + pelicula.puntuacion;
+                    puntuacionTextBox.Text = puntuacion.ToString();
+                }
+            }
+        }
+        private void nuevaPartidaButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(peliculas.Count >= 5)
+            {
+                numero = 0;
+                puntuacion = 0;
+                partida = new ObservableCollection<Pelicula>();
+                for(int i = 0; i < 5; i++)
+                {
+                    int aleatorio = NumAleatorio();
+                    partida.Add(peliculas[aleatorio]);
+                }
+                actualizar(partida[numero]);
+            }
+            else
+            {
+                MessageBox.Show("No hay suficientes peliculas en la base de datos","No son suficientes",MessageBoxButton.OK);
+            }
+        }
+        private void actualizar(Pelicula aux)
+        {
+            int numActual = numero + 1;
+            int numMax = partida.Count();
+            jugarDockPanel.DataContext = pelicula;
+            puntuacionTextBox.Text = puntuacion.ToString();
+            tituloTextBox.Text = "";
+            contadorTextBlock.Text = numActual.ToString() + "/" + numMax.ToString();
+        }
+        private int NumAleatorio()
+        {
+            int numero = rnd.Next(0, peliculas.Count + 1);
+            return numero;
         }
     }
 }
--- Juego_DINT/Pelicula.cs
+++ Juego_DINT/Pelicula.cs
@@ -1,25 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Juego_DINT
 {
-    class Pelicula
+    class Pelicula:INotifyPropertyChanged
     {
         public enum Genero 
         {
             Comedia,Miedo, Accion, Misterio
         }
-        string titulo { get; set; }
-        string pista { get; set; }
-        string imagen { get; set; }
-        bool facil { get; set; }
-        bool normal { get; set; }
-        bool dificil { get; set; }
-        Genero genero { get; set; }
-        int puntuacion { get; set; }
+        string _titulo;
+        public string titulo
+        {
+            get { return this._titulo; }
+            set
+            {
+                if(this._titulo != value)
+                {
+                    this._titulo = value;
+                    this.NotifyPropertychanged("titulo");
+                }
+            }
+        }
+        string _pista;
+        public string pista
+        {
+            get { return this._pista; }
+            set
+            {
+                if (this._pista != value)
+                {
+                    this._pista = value;
+                    this.NotifyPropertychanged("pista");
+                }
+            }
+        }
+        string _imagen;
+        public string imagen
+        {
+            get { return this._imagen; }
+            set
+            {
+                if (this._imagen != value)
+                {
+                    this._imagen = value;
+                    this.NotifyPropertychanged("imagen");
+                }
+            }
+        }
+
+        bool _facil;
+        public bool facil
+        {
+            get { return this._facil; }
+            set
+            {
+                if (this._facil != value)
+                {
+                    this._facil = value;
+                    this.NotifyPropertychanged("facil");
+                }
+            }
+        }
+        bool _normal;
+        public bool normal
+        {
+            get { return this._normal; }
+            set
+            {
+                if (this._normal != value)
+                {
+                    this._normal = value;
+                    this.NotifyPropertychanged("normal");
+                }
+            }
+        }
+        bool _dificil;
+        public bool dificil
+        {
+            get { return this._dificil; }
+            set
+            {
+                if (this._dificil != value)
+                {
+                    this._dificil = value;
+                    this.NotifyPropertychanged("dificil");
+                }
+            }
+        }
+        Genero _genero;
+        public Genero genero
+        {
+            get { return this._genero; }
+            set
+            {
+                if (this._genero != value)
+                {
+                    this._genero = value;
+                    this.NotifyPropertychanged("genero");
+                }
+            }
+        }
+        int _puntuacion;
+        public int puntuacion
+        {
+            get { return this._puntuacion; }
+            set
+            {
+                if (this._puntuacion != value)
+                {
+                    this._puntuacion = value;
+                    this.NotifyPropertychanged("puntuacion");
+                }
+            }
+        }
+
 
         public Pelicula() { }
 
@@ -36,6 +135,13 @@ namespace Juego_DINT
             if (facil == true) puntuacion = 10;
             else if (normal == true) puntuacion = 6;
             else puntuacion = 4;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertychanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(propertyName));
         }
     }
 }
